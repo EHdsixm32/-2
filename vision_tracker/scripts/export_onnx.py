66e57736098da8206b96d5c5e6e5d154afa0b1a2
EHdsixm32 <2755091435@qq.com>
2026-07-18 15:52:08 +0800

    Delete vision_tracker directory

--- vision_tracker/scripts/export_onnx.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('runs/train/target_detector/weights/best.pt')
-model.export(format='onnx', imgsz=640)
-print("ONNX exported to models/target.onnx")
--- vision_tracker/scripts/install_deps.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y cmake g++ git
-sudo apt install -y libopencv-dev libeigen3-dev libyaml-cpp-dev libspdlog-dev
